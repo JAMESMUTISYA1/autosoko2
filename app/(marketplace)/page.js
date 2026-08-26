3dc0a2762a16bb76b5e3fc6d2e6201128d07ef39
JAMESMUTISYA1 <ravetmu@gmail.com>
2026-08-26 09:52:33 +0300

    hh

--- app/(marketplace)/page.js
+++ app/(marketplace)/page.js
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import { Suspense } from "react";
 import { Search, MessageCircle, Truck, ShieldCheck } from "lucide-react";
 import HomeBanners from "@/components/HomeBanners";
